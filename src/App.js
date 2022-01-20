61498b1532094d36a038e5f6305a9753a07aad4c
Jacob K <jacobknighj@gmail.com>
2022-01-20 11:10:24 +1100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Account from './components/Account';
 
+checkStatus();
+function checkStatus() {
+  var config = {
+    method: 'get',
+    url: 'https://api.spacetraders.io/game/status',
+    headers: {}
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+function getAccount(token) {
+
+  var config = {
+    method: 'get',
+    url: 'https://api.spacetraders.io/my/account',
+    headers: {
+      'Authorization': token
+    }
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <Account />
+      <button onClick={checkStatus}>get status</button>
+
     </div>
   );
 }
---
+++ src/components/Account.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Account() {
+    return (
+        <div>
+            <p>yo</p>
+
+        </div>
+    )
+
+
+}
+export default Account
